94f8d9377292505324df267a9e120eeb46a80016
AjeetBind <bindajeet209@gmail.com>
2026-02-22 18:53:56 +0530

    update config.json file

--- cypress.config.js
+++ cypress.config.js
@@ -4,45 +4,32 @@ import allureWriter from "@shelex/cypress-allure-plugin/writer";
 export default defineConfig({
 
   video: true,
-  videoCompression: false,
   trashAssetsBeforeRuns: false,
 
-  reporter: "cypress-mochawesome-reporter",
+  reporter: "cypress-multi-reporters",
+
   reporterOptions: {
-    reportDir: "cypress/reports",
-    overwrite: false,
-    html: true,
-    json: true,
-    charts: true,
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    reportFilename: "index"
+    reporterEnabled: "cypress-mochawesome-reporter, junit",
+
+    cypressMochawesomeReporterReporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true
+    },
+
+    junitReporterOptions: {
+      mochaFile: "cypress/results/results-[hash].xml"
+    }
   },
 
   e2e: {
-    experimentalOriginDependencies: true,
     baseUrl: "https://appuat.franciscanecare.net/Login/Identifier?SchCode=DEMOIN",
-
-    pageLoadTimeout: 120000,
-    requestTimeout: 15000,
-    responseTimeout: 15000,
-
-    screenshotOnRunFailure: true,
 
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
       allureWriter(on, config);
       return config;
     },
-  },
-
-env: {
-  URL: "https://appuat.franciscanecare.net/Login/Identifier?SchCode=DEMOIN",
-  IDCARD_URL: "https://idcarduat.franciscanecare.net",
-},
-
-reporter: 'junit',
-reporterOptions: {
-  mochaFile: 'cypress/results/results-[hash].xml'
-}
+  }
 });
